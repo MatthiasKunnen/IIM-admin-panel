65ab372557e74e4a74fde3ddca69288acf1426b3
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-02-13 04:17:49 +0100

    Added temporary Main class

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import domain.Domaincontroller;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        new Domaincontroller();
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+}
