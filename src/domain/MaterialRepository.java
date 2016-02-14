b332e232e5ea7fa1f9f3a17e46b8e66d70b2d7d8
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-02-14 05:21:36 +0100

    Implemented object immutability and persistence

--- src/domain/MaterialRepository.java
+++ src/domain/MaterialRepository.java
@@ -2,7 +2,12 @@ package domain;
 
 import java.util.*;
 
+import exceptions.MaterialAlreadyExistsException;
+import exceptions.MaterialNotFoundException;
 import persistence.*;
+
+import static util.ImmutabilityHelper.copyCollectionDefensively;
+import static util.ImmutabilityHelper.copyDefensively;
 
 public class MaterialRepository {
 
@@ -25,18 +30,71 @@ public class MaterialRepository {
     //<editor-fold desc="Getters and setters" defaultstate="collapsed">
 
     public Set<Material> getMaterials() {
-        return Collections.unmodifiableSet(materials);
+        return (Set<Material>) copyCollectionDefensively(this.materials);
     }
 
     public Set<MaterialIdentifier> getMaterialIdentifiers() {
-        return Collections.unmodifiableSet(materialIdentifiers);
+        return (Set<MaterialIdentifier>) copyCollectionDefensively(this.materialIdentifiers);
     }
-    
-    public void addMaterial(Material material){
-        materials.add(material);
+    //</editor-fold>
+
+    //<editor-fold desc="Actions" defaultstate="collapsed">
+
+    public Material addMaterial(Material material) {
+        if (getMaterialById(material.getId()) != null)
+            throw new MaterialAlreadyExistsException(material);
+        Material toPersist = copyDefensively(material);
+        persistMaterial(toPersist);
+        addMaterialToCollections(toPersist);
+        return copyDefensively(toPersist);
     }
-    public void addMaterialIdentifier(MaterialIdentifier materialIdentifier){
-        materialIdentifiers.add(materialIdentifier);
+
+    public void removeMaterial(Material material) {
+        Material remove = getMaterialById(material.getId());
+        if (remove == null)
+            throw new MaterialNotFoundException(material);
+        persistence.remove(remove);
+        persistence.remove(remove.getIdentifiers());
+        removeMaterialFromCollections(remove);
+    }
+
+    public void update(Material material) {
+        if (material.getId() == 0)
+            throw new MaterialNotFoundException("Cannot update a record that does not appear in the database.", material);
+
+        Material toSave = copyDefensively(material);
+        Material original = getMaterialById(material.getId());
+
+        persistence.merge(toSave);
+
+        removeMaterialFromCollections(original);
+        addMaterialToCollections(toSave);
+    }
+
+    public Material getMaterialById(int id) {
+        return this.materials
+                .stream()
+                .filter(m -> m.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+
+    //</editor-fold>
+
+    //<editor-fold desc="Private actions" defaultstate="collapsed">
+    private void persistMaterial(Material material) {
+        persistence.persist(material);
+        persistence.persist(material.getIdentifiers());
+    }
+
+    private void addMaterialToCollections(Material material) {
+        this.materials.add(material);
+        this.materialIdentifiers.addAll(material.getIdentifiers());
+    }
+
+    private void removeMaterialFromCollections(Material material) {
+        this.materials.remove(material);
+        this.materialIdentifiers.removeAll(material.getIdentifiers());
     }
     //</editor-fold>
 }
