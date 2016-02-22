c96c3af25ad72bfdd53f7b5337f13739fdd63d0e
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-02-22 03:00:31 +0100

    Added MethodBuilder and renamed for conventions

--- src/gui/GuiHelper.java
+++ src/gui/GuiHelper.java
@@ -1,12 +1,20 @@
 package gui;
 
 import javafx.scene.control.TextField;
+import org.mockito.Mockito;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class GuiHelper {
-    private static Pattern numberOnlyPattern = Pattern.compile("[0-9]");
-    private static String DECIMAL_SEPARATOR = ",";
+    private final static Pattern NUMBER_ONLY_PATTERN = Pattern.compile("[0-9]");
+    private final static String DECIMAL_SEPARATOR = ",";
 
     public static void getKeyEventEventHandlerAssuringDecimalInput(TextField input) {
         input.setOnKeyTyped(event -> {
@@ -17,7 +25,7 @@ public class GuiHelper {
             }else if(!input.getText().contains(DECIMAL_SEPARATOR) && event.getCharacter().equals(".")){
                 input.insertText(input.getCaretPosition(), DECIMAL_SEPARATOR);
                 event.consume();
-            } else if (!numberOnlyPattern.matcher(event.getCharacter()).matches()) {
+            } else if (!NUMBER_ONLY_PATTERN.matcher(event.getCharacter()).matches()) {
                 event.consume();
             }
         });
@@ -25,7 +33,7 @@ public class GuiHelper {
 
     public static void getKeyEventEventHandlerAssuringIntegerInput(TextField input) {
         input.setOnKeyTyped(event -> {
-           if (!numberOnlyPattern.matcher(event.getCharacter()).matches()) {
+            if (!NUMBER_ONLY_PATTERN.matcher(event.getCharacter()).matches()) {
                 event.consume();
             }
         });
@@ -33,5 +41,93 @@ public class GuiHelper {
 
     private GuiHelper() {
 
+    }
+
+    public static MethodBuilder createMethodBuilder(Object source) {
+        return new MethodBuilder(source);
+    }
+}
+
+class MethodBuilder {
+    private Object source;
+    private Object defaultValue = null;
+    private List<MethodCall> callList = new ArrayList<>();
+    private MethodCall targetCall;
+    private Object targetObject;
+
+    public MethodBuilder(Object source) {
+        this.source = source;
+    }
+
+    public MethodBuilder setDefaultValue(Object defaultValue) {
+        this.defaultValue = defaultValue;
+        return this;
+    }
+
+    public MethodBuilder addMethods(String... methods) {
+        for (String method : methods) {
+            callList.add(new MethodCall(method));
+        }
+        return this;
+    }
+
+    public MethodBuilder addMethod(String method, Object... parameters) {
+        callList.add(new MethodCall(method, parameters));
+        return this;
+    }
+
+    public MethodBuilder setTarget(Object targetObject, String method, Object... parameters) {
+        this.targetCall = new MethodCall(method, parameters);
+        this.targetObject = targetObject;
+        return this;
+    }
+
+    public Object run() {
+        Object result = this.source;
+        for (MethodCall m : this.callList) {
+            result = invoke(result, m.getName(), m.getParameters());
+            if (result == null) {
+                return defaultValue;
+            }
+        }
+        if (targetCall != null && targetObject != null) {
+            List<Object> parameters = new ArrayList<>(Arrays.asList(targetCall.getParameters()));
+            parameters.add(0, result);
+            invoke(targetObject, targetCall.getName(), parameters.toArray(new Object[parameters.size()]));
+        }
+        return result;
+    }
+
+    private Object invoke(Object obj, String method, Object... parameters) {
+        try {
+            if (parameters.length == 0) {
+                return obj.getClass().getMethod(method).invoke(obj);
+            } else {
+                List<Class<?>> typeList = Arrays.stream(parameters).map(Object::getClass).collect(Collectors.toList());
+                Class[] types = typeList.toArray(new Class[typeList.size()]);
+                return obj.getClass().getMethod(method, types).invoke(obj, parameters);
+            }
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            return this.defaultValue;
+        }
+    }
+
+
+   private class MethodCall {
+        private String name;
+        private Object[] parameters;
+
+        public MethodCall(String name, Object... parameters) {
+            this.name = name;
+            this.parameters = parameters;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Object[] getParameters() {
+            return parameters;
+        }
     }
 }
