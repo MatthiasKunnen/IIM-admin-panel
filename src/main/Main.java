20732345182bff6a80df595f6c192b6c08b4ed93
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-02-29 23:11:35 +0100

    Formatted Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,11 +9,9 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-
     @Override
     public void start(Stage stage) {
-        DomainController domainController = new DomainController();
-        Scene scene = new Scene(new ReservationOverviewController(domainController));
+        Scene scene = new Scene(new ReservationOverviewController(new DomainController()));
         stage.setTitle("Login - IIM");
         stage.setScene(scene);
         stage.setResizable(false);
