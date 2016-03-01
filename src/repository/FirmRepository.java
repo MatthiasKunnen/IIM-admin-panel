65fe6612480466e2b7f2b3709cad04296a109fbc
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-03-01 05:22:29 +0100

    Fixed leaking in FirmRepo as addressed in #11

--- src/repository/FirmRepository.java
+++ src/repository/FirmRepository.java
@@ -7,15 +7,18 @@ import java.util.stream.Collectors;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import persistence.PersistenceEnforcer;
+import util.ImmutabilityHelper;
 
 
 public class FirmRepository extends Repository<Firm>{
 
     public FirmRepository(PersistenceEnforcer persistence) {
         super(persistence);
+        eList = persistence.retrieve(Firm.class);
+        eObservableList = FXCollections.observableList((List<Firm>) ImmutabilityHelper.copyCollectionDefensively(eList));
     }
 
     public ObservableList<Firm> getExistingFirms(){
-        return FXCollections.observableArrayList(this.eList.stream().collect(Collectors.toList()));
+        return FXCollections.unmodifiableObservableList(eObservableList);
     }
 }
