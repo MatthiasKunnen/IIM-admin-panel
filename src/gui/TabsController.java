e07cecdd16aa87aa10c82d207de328f46d779ea8
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-03-07 03:28:24 +0100

    Made TempController generic

--- src/gui/TabsController.java
+++ src/gui/TabsController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
 
 import domain.Curricular;
@@ -10,20 +5,14 @@ import domain.DomainController;
 import domain.Firm;
 import domain.TargetGroup;
 import exceptions.InvalidEmailException;
-import exceptions.InvalidPhoneNumberException;
-
-import java.io.IOException;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import org.controlsfx.control.textfield.CustomTextField;
+import org.apache.commons.validator.routines.EmailValidator;
+import java.io.IOException;
 
 /**
  * FXML Controller class
@@ -69,8 +58,8 @@ public class TabsController extends TabPane {
         TempController<Curricular> tcCurricular = createCurricularOverview();
         TempController<TargetGroup> tcTargetGroup = createTargetGroupOverview();
 
-        tcCurricular.getListView().maxHeightProperty().bind(tcFirm.getListView().heightProperty());
-        tcTargetGroup.getListView().maxHeightProperty().bind(tcFirm.getListView().heightProperty());
+        tcCurricular.getListViewMaxHeightProperty().bind(tcFirm.getListViewHeightProperty());
+        tcTargetGroup.getListViewMaxHeightProperty().bind(tcFirm.getListViewHeightProperty());
 
         overview.getChildren().add(tcFirm);
         overview.getChildren().add(tcCurricular);
@@ -80,304 +69,77 @@ public class TabsController extends TabPane {
     }
 
     private TempController<TargetGroup> createTargetGroupOverview() {
-        TempController<TargetGroup> tcTargetGroup = new TempController<>("Doelgroepen", dc.getTargetGroups());
-        CustomTextField tfName = new CustomTextField();
-        tfName.setPromptText("Naam");
-        Button btnAdd = new Button("Opslaan");
-        Button btnDelete = new Button("Verwijder");
-        Button btnCancel = new Button("Annuleer");
+        TempController<TargetGroup> tcTargetGroup = new TempController<>(
+                "Doelgroepen",
+                TargetGroup::getName,
+                dc.getTargetGroups());
 
-        btnCancel.setDisable(true);
-        btnCancel.setVisible(false);
-        btnDelete.setDisable(true);
-        btnDelete.setVisible(false);
-
-        tcTargetGroup.getListView().setCellFactory(param -> new ListCell<TargetGroup>() {
-            @Override
-            protected void updateItem(TargetGroup item, boolean empty) {
-                super.updateItem(item, empty);
-                if (item != null) {
-                    setText(String.format("%s", item.getName()));
-                }
-            }
-        });
-
-        tcTargetGroup.getListView().getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                btnDelete.setDisable(false);
-                btnDelete.setVisible(true);
-
-                btnCancel.setDisable(false);
-                btnCancel.setVisible(true);
-
-                tfName.setText(newValue.getName());
-            }
-
-        });
-
-        btnDelete.setOnAction((ActionEvent event) -> {
-            TargetGroup tg = tcTargetGroup.getListView().getSelectionModel().getSelectedItem();
-            dc.removeTargetGroup(tg);
-
-            tfName.clear();
-
-            btnDelete.setDisable(true);
-            btnDelete.setVisible(false);
-
-            btnCancel.setDisable(true);
-            btnCancel.setVisible(false);
-
-            tcTargetGroup.getListView().getSelectionModel().clearSelection();
-        });
-
-        btnCancel.setOnAction((ActionEvent event) -> {
-            tfName.clear();
-
-            btnDelete.setDisable(true);
-            btnDelete.setVisible(false);
-
-            btnCancel.setDisable(true);
-            btnCancel.setVisible(false);
-
-            tcTargetGroup.getListView().getSelectionModel().clearSelection();
-        });
-
-        btnAdd.setOnAction((ActionEvent event) -> {
-
-            if (tfName.getText().isEmpty()) {
-                GuiHelper.showError(tfName, "Een Doelgroep heeft een naam nodig!");
-            }
-            String name = tfName.getText();
-
-            TargetGroup tg = tcTargetGroup.getListView().getSelectionModel().isEmpty() ? new TargetGroup() : tcTargetGroup.getListView().getSelectionModel().getSelectedItem();
-
-            tg.setName(name);
-
-            if (tcTargetGroup.getListView().getSelectionModel().isEmpty()) {
-                dc.addTargetGroup(tg);
-            } else {
-
-                dc.updateTargetGroup(tg);
-            }
-
-            tfName.clear();
-        });
-
-        tcTargetGroup.addHorizontalButton(btnCancel);
-        tcTargetGroup.addHorizontalButton(btnAdd);
-        tcTargetGroup.addHorizontalButton(btnDelete);
-
-        tcTargetGroup.addVerticalNode(tfName);
+        tcTargetGroup.addManagementCustomTextField("name", new ManagedCustomTextFieldBuilder<TargetGroup>()
+                .setConverter(TargetGroup::getName)
+                .setPromptText("Naam")
+                .addErrorPredicate(name -> !name.isEmpty(), "Naam moet ingevuld worden!")
+                .get());
 
         return tcTargetGroup;
     }
 
     private TempController<Curricular> createCurricularOverview() {
-        TempController<Curricular> tcCurricular = new TempController<>("Leergebieden", dc.getCurricular());
-        CustomTextField tfName = new CustomTextField();
-        tfName.setPromptText("Naam");
-        Button btnAdd = new Button("Opslaan");
-        Button btnDelete = new Button("Verwijder");
-        Button btnCancel = new Button("Annuleer");
+        TempController<Curricular> tcCurricular = new TempController<>(
+                "Leergebieden",
+                Curricular::getName,
+                dc.getCurricular());
 
-        btnCancel.setDisable(true);
-        btnCancel.setVisible(false);
-        btnDelete.setDisable(true);
-        btnDelete.setVisible(false);
-
-        tcCurricular.getListView().setCellFactory(param -> new ListCell<Curricular>() {
-            @Override
-            protected void updateItem(Curricular item, boolean empty) {
-                super.updateItem(item, empty);
-                if (item != null) {
-                    setText(String.format("%s", item.getName()));
-                }
-            }
-        });
-
-        tcCurricular.getListView().getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                btnDelete.setDisable(false);
-                btnDelete.setVisible(true);
-
-                btnCancel.setDisable(false);
-                btnCancel.setVisible(true);
-
-                tfName.setText(newValue.getName());
-            }
-
-        });
-
-        btnDelete.setOnAction((ActionEvent event) -> {
-            Curricular c = tcCurricular.getListView().getSelectionModel().getSelectedItem();
-            dc.removeCurricular(c);
-
-            tfName.clear();
-
-            btnDelete.setDisable(true);
-            btnDelete.setVisible(false);
-
-            btnCancel.setDisable(true);
-            btnCancel.setVisible(false);
-
-            tcCurricular.getListView().getSelectionModel().clearSelection();
-        });
-
-        btnCancel.setOnAction((ActionEvent event) -> {
-            tfName.clear();
-
-            btnDelete.setDisable(true);
-            btnDelete.setVisible(false);
-
-            btnCancel.setDisable(true);
-            btnCancel.setVisible(false);
-
-            tcCurricular.getListView().getSelectionModel().clearSelection();
-        });
-
-        btnAdd.setOnAction((ActionEvent event) -> {
-
-            if (tfName.getText().isEmpty()) {
-                GuiHelper.showError(tfName, "Een leergebied heeft een naam nodig!");
-            }
-            String name = tfName.getText();
-
-            Curricular c = tcCurricular.getListView().getSelectionModel().isEmpty() ? new Curricular(tfName.getText()) : tcCurricular.getListView().getSelectionModel().getSelectedItem();
-
-            if (tcCurricular.getListView().getSelectionModel().isEmpty()) {
-                dc.addCurricular(c);
-            } else {
-                c.setName(name);
-                dc.updateCurricular(c);
-            }
-
-            tfName.clear();
-        });
-
-        tcCurricular.addHorizontalButton(btnCancel);
-        tcCurricular.addHorizontalButton(btnAdd);
-        tcCurricular.addHorizontalButton(btnDelete);
-
-        tcCurricular.addVerticalNode(tfName);
+        tcCurricular.addManagementCustomTextField("name", new ManagedCustomTextFieldBuilder<Curricular>()
+                .setConverter(Curricular::getName)
+                .setPromptText("Naam")
+                .addErrorPredicate(name -> !name.isEmpty(), "Naam moet ingevuld worden!")
+                .get());
 
         return tcCurricular;
     }
 
     private TempController<Firm> createFirmOverview() {
-        TempController<Firm> tcFirm = new TempController<>("Firmas", dc.getFirms());
-        CustomTextField tfFirmName = new CustomTextField();
-        tfFirmName.setPromptText("Naam");
-        CustomTextField tfFirmEmail = new CustomTextField();
-        tfFirmEmail.setPromptText("E-mail");
-        CustomTextField tfFirmPhone = new CustomTextField();
-        tfFirmPhone.setPromptText("Telefoon nummer");
-        Button btnFirmAdd = new Button("Opslaan");
-        Button btnFirmDelete = new Button("Verwijder");
-        Button btnFirmCancel = new Button("Annuleer");
+        TempController<Firm> tcFirm = new TempController<>("Firma's",
+                (Firm f) -> String.format("%s (%s)", f.getName(), f.getEmail()),
+                dc.getFirms());
 
-        btnFirmCancel.setDisable(true);
-        btnFirmCancel.setVisible(false);
-        btnFirmDelete.setDisable(true);
-        btnFirmDelete.setVisible(false);
+        tcFirm.addManagementCustomTextField("name", new ManagedCustomTextFieldBuilder<Firm>()
+                .setConverter(Firm::getName)
+                .setPromptText("Naam")
+                .addErrorPredicate(name -> !name.isEmpty(), "Naam moet ingevuld worden!")
+                .get());
+        tcFirm.addManagementCustomTextField("email", new ManagedCustomTextFieldBuilder<Firm>()
+                .setConverter(Firm::getEmail)
+                .setPromptText("E-mail")
+                .addErrorPredicate(email -> !email.isEmpty(), "E-mail moet ingevuld worden!")
+                .addErrorPredicate(email -> EmailValidator.getInstance().isValid(email), "E-mail is niet correct.")
+                .get());
+        tcFirm.addManagementCustomTextField("phone_number", new ManagedCustomTextFieldBuilder<Firm>()
+                .setConverter(Firm::getPhoneNumber)
+                .setPromptText("Telefoonnummer")
+                .addWarningPredicate(phoneNumber -> !phoneNumber.isEmpty(), "Telefoonnummer is niet ingevuld.")
+                .get());
 
-        tcFirm.getListView().setCellFactory(param -> new ListCell<Firm>() {
-            @Override
-            protected void updateItem(Firm item, boolean empty) {
-                super.updateItem(item, empty);
-                if (item != null) {
-                    setText(String.format("%s (%s)", item.getName(), item.getEmail()));
-                }
-            }
-        });
+        tcFirm.setOnAdd(()->{
+            boolean willSave = true;
+            String name = tcFirm.getValue("name");
+            String email = tcFirm.getValue("email");
+            String phone = tcFirm.getValue("phone_number");
 
-        tcFirm.getListView().getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                btnFirmDelete.setDisable(false);
-                btnFirmDelete.setVisible(true);
-
-                btnFirmCancel.setDisable(false);
-                btnFirmCancel.setVisible(true);
-
-                tfFirmName.setText(newValue.getName());
-                tfFirmPhone.setText(newValue.getPhoneNumber());
-                tfFirmEmail.setText(newValue.getEmail());
-            }
-
-        });
-
-        btnFirmDelete.setOnAction((ActionEvent event) -> {
-            Firm f = tcFirm.getListView().getSelectionModel().getSelectedItem();
-            dc.removeFirm(f);
-
-            tfFirmEmail.clear();
-            tfFirmName.clear();
-            tfFirmPhone.clear();
-
-            btnFirmDelete.setDisable(true);
-            btnFirmDelete.setVisible(false);
-
-            btnFirmCancel.setDisable(true);
-            btnFirmCancel.setVisible(false);
-
-            tcFirm.getListView().getSelectionModel().clearSelection();
-        });
-
-        btnFirmCancel.setOnAction((ActionEvent event) -> {
-            tfFirmEmail.clear();
-            tfFirmName.clear();
-            tfFirmPhone.clear();
-
-            btnFirmDelete.setDisable(true);
-            btnFirmDelete.setVisible(false);
-
-            btnFirmCancel.setDisable(true);
-            btnFirmCancel.setVisible(false);
-
-            tcFirm.getListView().getSelectionModel().clearSelection();
-        });
-
-        btnFirmAdd.setOnAction((ActionEvent event) -> {
-
-            if (tfFirmName.getText().isEmpty()) {
-                GuiHelper.showError(tfFirmName, "Een bedrijf heeft een naam nodig!");
-            }
-            String name = tfFirmName.getText();
-
-            String email = tfFirmEmail.getText();
-            String phone = tfFirmPhone.getText();
-
-            Firm f = tcFirm.getListView().getSelectionModel().isEmpty() ? new Firm() : tcFirm.getListView().getSelectionModel().getSelectedItem();
-
+            Firm newFirm = new Firm();
             try {
-                f.setEmail(email);
-                f.setName(name);
-                f.setPhoneNumber(phone);
-            } catch (InvalidPhoneNumberException ex) {
-                GuiHelper.showError(tfFirmPhone, ex.getMessage());
+                newFirm.setEmail(email);
+                newFirm.setName(name);
+                newFirm.setPhoneNumber(phone);
             } catch (InvalidEmailException ex) {
-                GuiHelper.showError(tfFirmEmail, ex.getMessage());
+                willSave = false;
             }
 
-            if (tcFirm.getListView().getSelectionModel().isEmpty()) {
-                dc.addFirm(f);
-            } else {
-                dc.updateFirm(f);
+            if (willSave){
+                dc.addFirm(newFirm);
             }
-
-            tfFirmEmail.clear();
-            tfFirmName.clear();
-            tfFirmPhone.clear();
+            return willSave;
         });
-
-        tcFirm.addHorizontalButton(btnFirmCancel);
-        tcFirm.addHorizontalButton(btnFirmAdd);
-        tcFirm.addHorizontalButton(btnFirmDelete);
-
-        tcFirm.addVerticalNode(tfFirmName);
-        tcFirm.addVerticalNode(tfFirmEmail);
-        tcFirm.addVerticalNode(tfFirmPhone);
-
         return tcFirm;
     }
-
 }
--- src/gui/TempController.java
+++ src/gui/TempController.java
@@ -1,27 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
 
 
-import java.beans.EventHandler;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BooleanSupplier;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import org.controlsfx.control.textfield.CustomTextField;
 
 
-public class TempController<E> extends AnchorPane{
+public class TempController<E> extends AnchorPane {
+
+    //<editor-fold desc="FXML Variables" defaultstate="collapsed">
+
     @FXML
     private Label lblTitel;
     @FXML
@@ -30,38 +35,213 @@ public class TempController<E> extends AnchorPane{
     private ListView<E> lvItems;
     @FXML
     private HBox hbButtons;
+    @FXML
+    private Button btnDelete;
+    @FXML
+    private Button btnSave;
+    @FXML
+    private Button btnCancel;
+    //</editor-fold>
 
-    public TempController(String titel,ObservableList<E> items){
+    //<editor-fold desc="Variables" defaultstate="collapsed">
+    private Map<String, ManagedCustomTextField<E>> managedCustomTextFields = new HashMap<>();
+    private Predicate<E> saveEvent;
+    private BooleanSupplier addEvent;
+
+    //</editor-fold>
+
+    //<editor-fold desc="Constructors" defaultstate="collapsed">
+    public TempController(String title, Function<E, String> stringPresentation, ObservableList<E> items) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Temp.fxml"));
         loader.setRoot(this);
         loader.setController(this);
         try {
             loader.load();
-
+            this.getStylesheets().add("/gui/style/form.css");
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
-        
+
         this.lvItems.setItems(items);
-        this.lblTitel.setText(titel);
-        
+        this.lblTitel.setText(title);
+        this.lvItems.setCellFactory(param -> new ListCell<E>() {
+            @Override
+            protected void updateItem(E item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item != null) {
+                    setText(stringPresentation.apply(item));
+                }
+            }
+        });
+        this.lvItems.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            boolean b = newValue == null;
+            btnCancel.setDisable(b);
+            btnCancel.setVisible(!b);
+            btnDelete.setDisable(b);
+            btnDelete.setVisible(!b);
+            if (!b) { //Something is selected
+                for(ManagedCustomTextField<E> tf: this.managedCustomTextFields.values()){
+                    tf.setText(tf.convert(newValue));
+                }
+            }
+        });
     }
-    
-    public void addVerticalNode(Node theNode){
-        this.vbNodes.getChildren().add(theNode);
-        VBox.setMargin(theNode, new Insets(5, 0, 5, 0));
+
+    //</editor-fold>
+
+    //<editor-fold desc="Public actions" defaultstate="collapsed">
+    public void addManagementCustomTextField(String key, ManagedCustomTextField managedCustomTextField) {
+        this.managedCustomTextFields.put(key, managedCustomTextField);
+        this.vbNodes.getChildren().add(managedCustomTextField);
     }
-    
-    public void addHorizontalButton(Node button){
-        this.hbButtons.getChildren().add(button);
-        HBox.setMargin(button, new Insets(0, 5, 0, 5));
+
+    public void setOnDelete(Function<E, Boolean> deleteEvent) {
+        btnDelete.setOnAction(event -> {
+            if (deleteEvent.apply(lvItems.getSelectionModel().getSelectedItem()))
+                clear();
+        });
     }
-    
-    public ListView<E> getListView(){
-        return this.lvItems;
+
+    public void setOnSave(Predicate<E> saveEvent) {
+        this.saveEvent = saveEvent;
     }
-    
-    
-    
-    
+
+    public void setOnAdd(BooleanSupplier addEvent) {
+        this.addEvent = addEvent;
+    }
+
+    public String getValue(String key) {
+        return managedCustomTextFields.get(key).getText();
+    }
+
+    public DoubleProperty getListViewMaxHeightProperty() {
+        return this.lvItems.maxHeightProperty();
+    }
+
+    public ReadOnlyDoubleProperty getListViewHeightProperty() {
+        return this.lvItems.heightProperty();
+    }
+
+    //</editor-fold>
+
+    //<editor-fold desc="Private actions" defaultstate="collapsed">
+    private void clear() {
+        managedCustomTextFields.values().forEach(TextInputControl::clear);
+        this.lvItems.getSelectionModel().clearSelection();
+    }
+
+    //</editor-fold>
+
+    //<editor-fold desc="FXML actions" defaultstate="collapsed">
+    @FXML
+    private void save(ActionEvent event) {
+        boolean exit = false;
+        for (ManagedCustomTextField tf : this.managedCustomTextFields.values()) {
+            if (!tf.validate())
+                exit = true;
+        }
+        if (exit) return;
+        if (this.lvItems.getSelectionModel().isEmpty() ?
+                addEvent.getAsBoolean() :
+                saveEvent.test(this.lvItems.getSelectionModel().getSelectedItem())) clear();
+    }
+
+    @FXML
+    private void cancel(ActionEvent event){
+        clear();
+    }
+    //</editor-fold>
 }
+
+//<editor-fold desc="Classes" defaultstate="collapsed">
+class ManagedCustomTextFieldBuilder  <E>  {
+    private ManagedCustomTextField<E> ctf = new ManagedCustomTextField<>();
+
+    public ManagedCustomTextFieldBuilder<E> addWarningPredicate(Predicate<String> p, String warning) {
+        ctf.addWarningPredicate(p, warning);
+        return this;
+    }
+
+    public ManagedCustomTextFieldBuilder<E> addErrorPredicate(Predicate<String> p, String error) {
+        ctf.addErrorPredicate(p, error);
+        return this;
+    }
+
+    public ManagedCustomTextFieldBuilder<E> setPromptText(String text) {
+        ctf.setPromptText(text);
+        return this;
+    }
+
+    public ManagedCustomTextFieldBuilder<E> setConverter(Function<E, String> converter) {
+        ctf.setConverter(converter);
+        return this;
+    }
+
+    public ManagedCustomTextField<E> get() {
+        return ctf;
+    }
+}
+
+class ManagedCustomTextField<E> extends CustomTextField {
+    private List<Validation> warnings, errors;
+    private Function<E, String> converter;
+
+    public ManagedCustomTextField() {
+        this.warnings = new ArrayList<>();
+        this.errors = new ArrayList<>();
+    }
+
+    public void addWarningPredicate(Predicate<String> p, String message) {
+        warnings.add(new Validation(p, message));
+    }
+
+    public void addErrorPredicate(Predicate<String> p, String message) {
+        errors.add(new Validation(p, message));
+    }
+
+    public void setConverter(Function<E, String> converter) {
+        this.converter = converter;
+    }
+
+    public String convert(E e) {
+        return hasConverter() ? null : this.converter.apply(e);
+    }
+
+    public boolean hasConverter() {
+        return this.converter == null;
+    }
+
+    public boolean validate() {
+        GuiHelper.hideError(this);
+        for (Validation v : errors) {
+            if (!v.getPredicate().test(getText())) {
+                GuiHelper.showError(this, v.getMessage());
+                return false;
+            }
+        }
+        warnings.stream()
+                .filter(v -> !v.getPredicate().test(getText()))
+                .forEach(v -> GuiHelper.showWarning(this, v.getMessage()));
+        return true;
+    }
+}
+
+class Validation {
+    private Predicate<String> p;
+    private String message;
+
+    public Validation(Predicate<String> p, String message) {
+        this.p = p;
+        this.message = message;
+    }
+
+    public Predicate<String> getPredicate() {
+        return p;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
+
+//</editor-fold>
