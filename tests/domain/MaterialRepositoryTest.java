f936323fd163b618180086911c97a6d26b3e6b83
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-03-10 04:19:48 +0100

    Commented invalid code @mattiesvzw please review

--- tests/domain/MaterialRepositoryTest.java
+++ tests/domain/MaterialRepositoryTest.java
@@ -1,48 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domain;
 
-import java.util.Arrays;
-import java.util.Collection;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runners.Parameterized;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import persistence.PersistenceEnforcer;
-import repository.MaterialRepository;
 
 /**
- *
  * @author matthiasseghers
  */
 public class MaterialRepositoryTest {
-    
-    private static final String name="Wereldbol";
+
+    private static final String name = "Wereldbol";
     private Material material;
-    
+
     //private DomainController dc;
     @Mock
     private PersistenceEnforcer persistenceEnforcer;
 //    private MaterialRepository materialRepository;
 
-     public MaterialRepositoryTest()
-    {}
-     @Before
+    public MaterialRepositoryTest() {
+    }
+
+    @Before
     public void before() {
-        MockitoAnnotations.initMocks(this); 
+        MockitoAnnotations.initMocks(this);
 //        materialRepository= new MaterialRepository(PersistenceEnforcer);
 //        dc= new DomainController();
-        material=new Material(name);
+        material = new Material(name);
     }
+
     @Test
-    public void testAddMaterial(){
-        Mockito.when(persistenceEnforcer.persist(material));
+    public void testAddMaterial() {
+        //Mockito.when(persistenceEnforcer.persist(material));
     }
 //    @Test
 //    public void testAddMaterial(){
