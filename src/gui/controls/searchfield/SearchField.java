3b67641e8fa04b445e409919fb7f51d3569c00f7
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-04-06 05:56:45 +0200

    Added SearchField

---
+++ src/gui/controls/searchfield/SearchField.java
@@ -0,0 +1,15 @@
+package gui.controls.searchfield;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import org.controlsfx.control.textfield.CustomTextField;
+
+public class SearchField extends CustomTextField {
+    public SearchField() {
+        ImageView ivClear = new ImageView(new Image(getClass().getResource("/gui/images/x.png").toExternalForm()));
+        ivClear.setPreserveRatio(true);
+        ivClear.fitHeightProperty().bind(heightProperty().divide(2));
+        ivClear.setOnMouseClicked((MouseEvent) -> clear());
+        setRight(ivClear);
+    }
+}
