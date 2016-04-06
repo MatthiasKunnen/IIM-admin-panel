18847df275044f2d4301262a85882227f92eee20
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-04-06 17:20:25 +0200

    Set Cursor.HAND on SearchField clear icon

--- src/gui/controls/searchfield/SearchField.java
+++ src/gui/controls/searchfield/SearchField.java
@@ -1,5 +1,6 @@
 package gui.controls.searchfield;
 
+import javafx.scene.Cursor;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.controlsfx.control.textfield.CustomTextField;
@@ -8,6 +9,7 @@ public class SearchField extends CustomTextField {
     public SearchField() {
         ImageView ivClear = new ImageView(new Image(getClass().getResource("/gui/images/x.png").toExternalForm()));
         ivClear.setPreserveRatio(true);
+        ivClear.setCursor(Cursor.HAND);
         ivClear.fitHeightProperty().bind(heightProperty().divide(2));
         ivClear.setOnMouseClicked((MouseEvent) -> clear());
         setRight(ivClear);
